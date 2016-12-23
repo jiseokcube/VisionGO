100f7e927dc59e3529a293e10f762029b10ab4c6
Jiseok Choi <jiseokcube@gmail.com>
2016-12-22 19:20:44 -0500

    Added FXML application for Scene Builder

--- VisionGO/src/application/Main.java
+++ VisionGO/src/application/Main.java
@@ -1,24 +1,22 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("Application.fxml"));
+    
+        Scene scene = new Scene(root, 300, 275);
+    
+        stage.setTitle("VisionGO");
+        stage.setScene(scene);
+        stage.show();
+    }
 	
 	public static void main(String[] args) {
 		launch(args);
