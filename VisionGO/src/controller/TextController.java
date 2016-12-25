0ebf603edb0b87d14239e3ef585b2ce8dad98023
Chloe Choi <lynnecc98@gmail.com>
2016-12-25 23:29:28 +0900

    started on textController

--- VisionGO/src/controller/TextController.java
+++ VisionGO/src/controller/TextController.java
@@ -1,5 +1,5 @@
 /*
- * File Name: GameController.java
+ * File Name: TextController.java
  * Project: VisionGO
  * Author: Chloe Choi
  * Start Date: 12/25/2016
@@ -9,6 +9,36 @@
 
 package controller;
 
+import model.Board;
+import model.Stone;
+
+import java.util.ArrayList;
+
 public class TextController {
-	
+    private String player = "Black";
+    private int numTurns = 1;
+    private ArrayList<Stone> movesMade = new ArrayList<Stone>();
+    private int capturedByWhite = 0;
+    private int capturedByBlack = 0;
+    private boolean skippedOnce = false;
+    private Board currentBoard = new Board();
+    private Board previousBoard;
+    private boolean resign = false;
+
+    public void start() throws Exception {
+        System.out.println("Welcome to VisionGO");
+        System.out.println(currentBoard.getBoard());
+        play();
+    }
+
+    public void play() {
+
+        while (!resign) {
+           /*
+            (\_/)
+            (o.o)!! 안뇽
+            (___)0
+            */
+        }
+    }
 }
