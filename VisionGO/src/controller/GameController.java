0ac535c3148028ca16d3ae75d173f9d2c73446b8
Chloe Choi <lynnecc98@gmail.com>
2016-12-25 19:01:35 +0900

    game controller class added

---
+++ VisionGO/src/controller/GameController.java
@@ -0,0 +1,15 @@
+package controller;
+
+/*
+ * File Name: GameController.java
+ * Project: VisionGO
+ * Author: Chloe Choi
+ * Start Date: 12/25/2016
+ * End Date:
+ * Description: This is the class that outputs the game in text format.
+ */
+
+public class GameController {
+
+
+}
