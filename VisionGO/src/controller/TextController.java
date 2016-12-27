642df35a09a0cd9d4523bb37d8c7308d679777f1
Chloe Choi <lynnecc98@gmail.com>
2016-12-27 23:19:49 +0900

    roughly finished TextController

--- VisionGO/src/controller/TextController.java
+++ VisionGO/src/controller/TextController.java
@@ -7,64 +7,98 @@
  * Description: This is the class that outputs the game in text format.
  */
 
+
+/*
+ *(\_/)
+ *(o.o)!!! 안뇽
+ *(___)0
+ */
 package controller;
 
 import model.Board;
+import model.InvalidMoveException;
 import model.Stone;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TextController {
-    private String player = "Black";
-    private int numTurns = 1;
-    private ArrayList<Stone> movesMade = new ArrayList<Stone>();
-    private int capturedByWhite = 0;
-    private int capturedByBlack = 0;
-    private boolean skippedOnce = false;
-    private Board currentBoard = new Board();
-    private Board previousBoard;
-    private boolean resign = false;
+    //player variable also works as numTurns
+    private static int player = 1;
+    private static ArrayList<Stone> movesMade = new ArrayList<Stone>();
+    private static Board currentBoard = new Board();
+    private static Board previousBoard;
+    private static boolean resign = false;
+    private static boolean skippedOnce = false;
     private static Scanner keyboard = new Scanner(System.in);
 
-    public void start() throws Exception {
+    public static void start() throws Exception {
         System.out.println("Welcome to VisionGO");
-        System.out.println(currentBoard.getBoard());
+
         play();
     }
 
-    public void play() {
-
+    public static void play() {
         while (!resign) {
+            System.out.println(currentBoard.getBoard());
+            if (player % 2 != 0) {
+                System.out.println("Black's Turn:");
+            } else {
+                System.out.println("White's Turn:");
+            }
             System.out.println("What would you like to do?\n"
                     + "1. Place Stone\n"
                     + "2. Skip\n"
                     + "3. Resign\n");
             int response = keyboard.nextInt();
             keyboard.nextLine();
-            switch (response){
+            switch (response) {
                 case 1:
-
+                    place();
                     break;
                 case 2:
-
+                    if (skippedOnce) {
+                        end(currentBoard.determineWinner());
+                    } else {
+                        skippedOnce = true;
+                        player++;
+                    }
                     break;
                 case 3:
                     resign = true;
-                    end();
+                    end(player++);
                     break;
                 default:
                     break;
             }
-
-           /*
-            (\_/)
-            (o.o)!! 안뇽
-            (___)0
-            */
         }
     }
-    public void end(){
 
+    public static void place() {
+        previousBoard = currentBoard;
+        System.out.println("Where do you want to place your stone?\n"
+                + "Row: ");
+        int row = keyboard.nextInt();
+        keyboard.nextLine();
+        System.out.println("Column: ");
+        int col = keyboard.nextInt();
+        keyboard.nextLine();
+        try {
+            currentBoard.makeMove(row, col);
+            skippedOnce = false;
+            player++;
+        } catch (InvalidMoveException e) {
+            place();
+        }
+    }
+
+    public static void end(int winner) {
+        String color;
+        if (winner % 2 == 0) {
+            color = "White";
+        } else {
+            color = "Black";
+        }
+        System.out.println(color + " wins\nScore: " + currentBoard.calculateScore());
     }
 }
